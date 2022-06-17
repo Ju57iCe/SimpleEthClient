2323b976089cf1ace4100c6a78df63c86b7e977b
Ju57iCe <st.z.atanasov@gmail.com>
2022-06-17 22:09:58 +0300

    Added boost program_options for handling cmd arguments

--- main.cpp
+++ main.cpp
@@ -2,11 +2,37 @@
  * Copyright Soramitsu Co., Ltd. All Rights Reserved.
  * SPDX-License-Identifier: Apache-2.0
  */
+#include <iostream>
+#include <boost/program_options.hpp>
 
 #include "ClientApp.h"
 
 int main(int argc, char *argv[])
 {
-    ClientApp client("/ip4/127.0.0.1/tcp/3333");
+    boost::program_options::options_description desc
+        ("\nInvocation : <program> --multiaddress <multiaddress_string>\nAgruments");
+
+    desc.add_options ()
+    ("multiaddress", boost::program_options::value<std::string>()->required(),
+                 "* Multiaddress.");
+
+    boost::program_options::variables_map vm;
+
+    try {
+        boost::program_options::store(boost::program_options::command_line_parser(argc, argv).
+                                                              options(desc).
+                                                              run(), vm);
+        boost::program_options::notify(vm);
+
+    } catch (boost::program_options::error& e) {
+        std::cout << "ERROR: " << e.what() << "\n";
+        std::cout << desc << "\n";
+        return 1;
+    }
+
+    std::string multiaddress(vm["multiaddress"].as<std::string>());
+    std::cout << "Client multiaddress : " << multiaddress << std :: endl;
+
+    ClientApp client(multiaddress);
     client.run();
 }
