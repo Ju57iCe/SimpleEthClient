7944016ba55cbe8f5e76680eee6bd16a84358163
Ju57iCe <st.z.atanasov@gmail.com>
2022-07-22 22:31:40 +0300

    Added MPT interface declarations Added initial MPT test cases Restructured tests project to separate RLP tests from MPT tests

--- libs/Utils/MPT.h
+++ libs/Utils/MPT.h
@@ -18,10 +18,17 @@ private:
         bool has_branches = false;
         uint32_t value = 0;
         std::string shared_nibbles;
+        std::string hash;
         std::array<std::unique_ptr<Node>, 16> branches;
     };
 public:
     void add_node(std::string entry, uint64_t value);
+    void get_node(std::string entry);
+    void delete_node(std::string entry);
+
+    void generate_proof(std::string entry);
+    void verify_proof(std::string entry);
+
     void print_contents();
 private:
     std::tuple<bool, uint64_t, MPT::Node*> find_parent(std::string key, MPT::Node* node, uint64_t total_nibbles_matched = 0);
---
+++ tests/mpt_tests.cpp
@@ -0,0 +1,43 @@
+#include <gtest/gtest.h>
+
+#include <vector>
+#include <any>
+#include <cstdint>
+
+#include "../libs/Utils/RLP.h"
+#include "../libs/Utils/RLPConstants.h"
+
+TEST(MPT, GetNotExistingKey)
+{
+    ASSERT_EQ(0, 0);
+}
+
+TEST(MPT, GetValueFromExistingKey)
+{
+    ASSERT_EQ(0, 0);
+}
+
+TEST(MPT, UpdateKeyValue)
+{
+    ASSERT_EQ(0, 0);
+}
+
+TEST(MPT, TrieHashChanging)
+{
+    ASSERT_EQ(0, 0);
+}
+
+TEST(MPT, SameNodeSameTrieHash)
+{
+    ASSERT_EQ(0, 0);
+}
+
+TEST(MPT, GenerateKeyProof)
+{
+    ASSERT_EQ(0, 0);
+}
+
+TEST(MPT, VerifyKeyProof)
+{
+    ASSERT_EQ(0, 0);
+}
